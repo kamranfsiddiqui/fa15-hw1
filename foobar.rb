d82d903f70ddb6774fc2d83af8c4118be6a5069d
Kamran Siddiqui <kamranfsiddiqui@berkeley.edu>
2015-09-18 11:51:46 -0700

    HW1 done

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,13 @@
 class Foobar
   # Q4 CODE HERE
+
+  def self.baz(a)
+     b = a.map{|i| i.to_i}
+     b = b.map{|i| i + 2}
+     b =b.select{|i| i % 2 == 0}
+     b = b.uniq
+     b=b.select{|i| i < 10}
+     b.inject{|s,x| s + x}     
+  end  
+
 end
